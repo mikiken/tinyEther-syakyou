0006f994005e582dbe4a12ed9d8fddf118acc08e
Kentaro Miki <mikiken.dev@gmail.com>
2024-03-08 00:16:49 +0900

    Add module to implement FRAME_FIFO

---
+++ sync_2ff.v
@@ -0,0 +1,20 @@
+// generic purpose 2-FF synchronizer
+
+module sync_2ff(
+        input  wire clk,
+        input  wire din,
+        output wire dout
+    );
+
+    reg temp1;
+    reg temp2;
+
+    assign dout = temp2;
+
+    always @(posedge clk)
+    begin
+        temp1 <= din;
+        temp2 <= temp1;
+    end
+
+endmodule
